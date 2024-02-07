0c281902346a23c0cba9da410677138c73c84c9c
Demis Bellot <demis.bellot@gmail.com>
2024-02-07 18:48:16 +0800

    Remove Swagger UI - can be added with x mix openapi3

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,25 +1,10 @@
-﻿using MyApp.ServiceInterface;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
-
-services.AddServiceStack(typeof(MyServices).Assembly, c =>
-{
-    c.AddSwagger();
-});
-
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
