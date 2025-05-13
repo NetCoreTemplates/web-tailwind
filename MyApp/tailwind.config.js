8e54d40826c7c8aecc2c43f4abd64e92cccae1ec
Demis Bellot <demis.bellot@gmail.com>
2025-05-14 02:25:07 +0800

    Upgrade to tailwindcss v4

--- MyApp/tailwind.config.js
+++ MyApp/tailwind.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  content: ["./wwwroot/**/*.{html,js,mjs,razor,cshtml}"],
+export default {
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'accent-1': '#FAFAFA',
+        'accent-2': '#EAEAEA',
+        danger: 'rgb(153 27 27)',
+        success: 'rgb(22 101 52)',
+      },
+    },
+  }
 }
